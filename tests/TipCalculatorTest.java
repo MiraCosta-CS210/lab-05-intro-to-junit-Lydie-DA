15516ba4546d667ff6a08fb71a35b5a387c16ce5
Lydie-DA <lydieduseanthony@gmail.com>
2025-02-26 11:22:27 -0800

    Update TipCalculatorTest.java

--- tests/TipCalculatorTest.java
+++ tests/TipCalculatorTest.java
@@ -1,2 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TipCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+     void calculateTipTest1() {
+        TipCalculator calculator = new TipCalculator();
+        double result = calculator.calculateTip(100,20);
+        assertEquals(20,result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateTipTest2() {
+        TipCalculator calculator = new TipCalculator();
+        double result = calculator.calculateTip(53,15);
+        assertEquals(7.95,result);
+    }
 }
